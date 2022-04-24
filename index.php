605a8ded235c441739ac683b8ee8791e6d552fb5
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-04-24 23:33:13 +0800

    Add images

--- index.php
+++ index.php
@@ -4,9 +4,31 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title> Quezon City University </title>
 </head>
 <body>
-   <h1> Hello world </h1>
+   <!-- header --> 
+   <div class="header">
+      <div class="h-logo">
+         <img src="img/svg/logo.svg" alt="">
+      </div>
+
+      <nav>
+         <ul class="main-nav">
+            <li> <a href="#"> Home </a> </li>
+            <li> <a href="#"> Contact </a> </li>
+            <li> <a href="#"> About </a> </li>
+         </ul>
+
+         <a href="#" class="s-nav"> Register </a>
+      </nav>
+   </div>
+
+   <div class="front-page">
+      <img src="./img/bg1.jpg" alt="">
+   </div>
+   
+
+
 </body>
 </html>
