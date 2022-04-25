7b7e771f43ccc8228f16b2dd451007b3b5c350ea
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-04-25 09:49:53 +0800

    Homepage

--- index.php
+++ index.php
@@ -4,31 +4,200 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./css/styles.css">
    <title> Quezon City University </title>
 </head>
 <body>
+
+   <div class="button-fixed">
+      <p> Shop now </p>
+   </div>
+
    <!-- header --> 
    <div class="header">
-      <div class="h-logo">
-         <img src="img/svg/logo.svg" alt="">
+      <div class="header-content">
+         <div class="h-logo">
+            <img src="img/svg/logo.svg" alt="">
+         </div>
+
+         <nav>
+            <ul class="main-nav">
+               <li> <a href="#"> Home </a> </li>
+               <li> <a href="#"> Contact </a> </li>
+               <li> <a href="#"> About </a> </li>
+            </ul>
+
+            <a href="#" class="s-nav"> Register </a>
+         </nav>
+      </div>
+   </div>
+   <!--x header -->
+
+   <!-- first page -->
+   <div class="front-page">
+      <div class="front-img">
+         <div class="front-overlay">  aa </div>
+         <img src="./img/bg1.jpg" alt="">
+         
+      </div>
+     
+
+      <div class="text-hero">
+         <h1> Welcome to Quezon City University </h1>
+         <p> Good life Starts here </p>
+      </div>
+      <div class="button-hero">
+         <a href="#"> Register now </a>
+      </div>
+   </div>
+   <!--x first page -->
+
+
+   <!-- courses -->
+   <div class="course-offer-container">
+      <div class="course-offer-box">
+
+         <!-- radio button start -->
+            <input type="radio" name="radio-btn" id="radio1">
+            <input type="radio" name="radio-btn" id="radio2">
+            <input type="radio" name="radio-btn" id="radio3">
+            <input type="radio" name="radio-btn" id="radio4">
+            <input type="radio" name="radio-btn" id="radio5">
+         <!-- radio button end -->
+
+         <!-- slides here -->
+         <div class="course-offer first">          
+            <div class="course-head">
+               <img src="./img/course head/it.png" alt="">
+               <div class="head-info">
+                     <h3> Dr. Isagani M. Tano </h3>
+                     <p> Dean, College of Computer Science and Information Technology </p>
+               </div>
+            </div>
+
+            <div class="about-course">
+               <div class="course-offer-banner">
+                  <h2> Course offer </h2>
+               </div>
+               <div class="course-title"> <h2> Bachelor of Science in Information Technology </h2></div>
+               <div class="course-desc"> 
+                  <p> The Bachelor of Science in Information Technology (BSIT) program prepares students to be IT professionals who are able to perform installation,operation, development, maintenance and administration of computer applications. </p>
+               </div>
+            </div>
+         </div>
+
+         <div class="course-offer">          
+            <div class="course-head">
+               <img src="./img/course head/it.png" alt="">
+               <div class="head-info">
+                     <h3> Dr. Isagani M. Tano </h3>
+                     <p> Dean, College of Computer Science and Information Technology </p>
+               </div>
+            </div>
+
+            <div class="about-course">
+               <div class="course-offer-banner">
+                  <h2> Course offer </h2>
+               </div>
+               <div class="course-title"> <h2> Bachelor of Science in Information Technology </h2></div>
+               <div class="course-desc"> 
+                  <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique facilis, cumque soluta quia sapiente dignissimos molestias molestiae repellendus commodi aspernatur necessitatibus laboriosam? Delectus, quaerat!</p>
+               </div>
+            </div>
+         </div>
+         <!--xx slides here -->
+
+         <!--automatic navigation -->
+         <div class="navigation-auto">
+            <div class="auto-btn1"> </div>
+            <div class="auto-btn2"> </div>
+            <div class="auto-btn3"> </div>
+            <div class="auto-btn4"> </div>
+            <div class="auto-btn5"> </div>
+         </div>
+         <!--xx automatic navigation -->
+      </div>
+         <!-- manual navigation -->
+         <div class="navigation-manual">
+            <label for="radio1" class="manual-btn"> </label>
+            <label for="radio2" class="manual-btn"> </label>
+            <label for="radio3" class="manual-btn"> </label>
+            <label for="radio4" class="manual-btn"> </label>
+            <label for="radio5" class="manual-btn"> </label>
+         </div>
+         <!--xx manual navigation -->
+
+   </div>
+   <!--x courses -->
+
+   <!-- shop -->
+   <div class="shop-container">
+      <img src="./img/shopbg.jpg" class="shopbg">
+
+      <div class="shop-content">
+         <div class="shop-text">
+            <h1> Be presentable </h1>
+            <p> Always wear complete uniform </p>
+
+            <a href="#"> 
+               <i> icon here</i> Shop now
+            </a>
+         </div>
+         <div class="shop-img">
+            <img src="./img/shop hero.png" alt="">
+         </div>
+      </div>
+   </div>
+   <!--x shop -->
+
+   <!-- campuses -->
+   <div class="campus-container">
+      <div class="campus-head"> 
+         <h1> Campuses </h1>
       </div>
 
-      <nav>
-         <ul class="main-nav">
-            <li> <a href="#"> Home </a> </li>
-            <li> <a href="#"> Contact </a> </li>
-            <li> <a href="#"> About </a> </li>
-         </ul>
+      <div class="campuses">
 
-         <a href="#" class="s-nav"> Register </a>
-      </nav>
+         <div class="campus-box">
+            <div class="camp-img">
+               <img src="./img/51948995_2224700194217155_7121890052690411520_n.jpg" alt="">
+            </div>
+            <div class="campus-info">
+               <div class="title"> 
+                  <h2> Campus name </h2>
+                  <p> Campus address </p>
+               </div>
+            </div>
+         </div>
+
+      </div>
+
    </div>
+   <!--x campuses -->
 
-   <div class="front-page">
-      <img src="./img/bg1.jpg" alt="">
+
+   <!-- footer -->
+   <div class="footer">
+      <div class="icon-links">
+            <ul>
+               <li> <a href="#" ><img src="./img/Icons/wtwitter.png" alt=""> </a> </li>
+               <li> <a href="#" ><img src="./img/Icons/wfacebook.png" alt=""> </a></li>
+               <li> <a href="#" ><img src="./img/Icons/winstagram.png" alt=""></a> </li>
+            </ul>
+      </div>
+
+      <div class="footer-text">
+         <p>  673 Quirino Highway, San Bartolome, Novaliches, Quezon City </p>
+         <p>  (02) 8806-3049 </p>
+      </div>
+
+      <div class="copyright">
+         <p> &copy; 2022 Quezon City University </p> 
+      </div>
    </div>
-   
-
+   <!--x footer -->
 
 </body>
+
+<script src="./script/slide.js"></script>
 </html>
