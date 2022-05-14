e9d5ab42780f8664dcfeb17942bca3a655056639
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-14 08:08:02 +0800

    Update qcu_ereg.sql

--- qcu_ereg.sql
+++ qcu_ereg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2022 at 04:58 AM
+-- Generation Time: May 14, 2022 at 02:06 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin_account` (
 --
 
 INSERT INTO `admin_account` (`id`, `username`, `password`, `firstname`, `lastname`, `avatar`) VALUES
-(1, 'qcu_admin', 'cf774e9e17ce451ffb528342bc256f2f', 'QCU', 'Admin', 'sample.jpg');
+(1500021, 'admin_mark', 'admin1234', 'Mark', 'Bacabis', 'profile.png');
 
 -- --------------------------------------------------------
 
@@ -67,6 +67,18 @@ INSERT INTO `admin_activity` (`id`, `username`, `activities`, `date`, `time`) VA
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `applicant_status`
+--
+
+CREATE TABLE `applicant_status` (
+  `id` int(11) NOT NULL,
+  `reg_num` varchar(55) NOT NULL,
+  `app_num` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `approved_applicant`
 --
 
@@ -76,10 +88,18 @@ CREATE TABLE `approved_applicant` (
   `app_num` varchar(55) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `gender` varchar(11) NOT NULL,
-  `brithdate` varchar(55) NOT NULL,
+  `birthdate` varchar(55) NOT NULL,
   `email` varchar(55) NOT NULL,
+  `schedule` varchar(55) NOT NULL,
   `date_approval` varchar(55) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `approved_applicant`
+--
+
+INSERT INTO `approved_applicant` (`id`, `reg_num`, `app_num`, `fullname`, `gender`, `birthdate`, `email`, `schedule`, `date_approval`) VALUES
+(27, 'REG202201', 'APP202202', 'Mark Melvin E Bacabis ', 'M', '2022-05-18', ' mark.melvin.bacabis@gmail.com', '2020-05-16 1:00PM-2:00PM', '22-05-13 09:51:26 PM');
 
 -- --------------------------------------------------------
 
@@ -90,8 +110,7 @@ CREATE TABLE `approved_applicant` (
 CREATE TABLE `schedule` (
   `schedID` int(11) NOT NULL,
   `Date` date NOT NULL,
-  `StartTime` varchar(10) NOT NULL,
-  `EndTime` varchar(10) NOT NULL,
+  `StartTime` varchar(55) NOT NULL,
   `Slot` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,13 +118,15 @@ CREATE TABLE `schedule` (
 -- Dumping data for table `schedule`
 --
 
-INSERT INTO `schedule` (`schedID`, `Date`, `StartTime`, `EndTime`, `Slot`) VALUES
-(1, '2022-05-16', '10:00AM', '11:00AM', 10),
-(2, '2022-05-16', '9:00AM', '10:00AM', 10),
-(3, '2022-05-16', '11:00AM', '12:00PM', 10),
-(4, '2022-05-16', '12:00PM', '1:00PM', 10),
-(5, '2022-05-16', '1:00PM', '2:00PM', 10),
-(6, '2022-05-16', '2:00PM', '3:00PM', 10);
+INSERT INTO `schedule` (`schedID`, `Date`, `StartTime`, `Slot`) VALUES
+(1, '2020-05-16', '9:00AM-10:00AM', 10),
+(2, '2020-05-16', '10:00AM-11:00AM', 10),
+(3, '2020-05-16', '12:00PM-1:00PM', 10),
+(4, '2020-05-16', '1:00PM-2:00PM', 10),
+(5, '2020-05-16', '2:00PM-3:00PM', 10),
+(6, '2022-05-18', '9:00AM-10:00AM', 9),
+(7, '2022-05-20', '9:00AM-10:00AM', 0),
+(8, '2022-05-25', '9:00AM-10:00AM', 4);
 
 -- --------------------------------------------------------
 
@@ -125,7 +146,31 @@ CREATE TABLE `stud_account` (
 --
 
 INSERT INTO `stud_account` (`id`, `app_num`, `email`, `stud_password`) VALUES
-(1, 'APP202305', 'marlon.union.eballes@gmail.com', '787d5f05953ec39b108869dfdd7733e6');
+(23, 'APP202202', ' mark.melvin.bacabis@gmail.com', 'd@tIYHh1G$CK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stud_address`
+--
+
+CREATE TABLE `stud_address` (
+  `id` int(11) NOT NULL,
+  `reg_num` varchar(55) NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `houseNo` int(11) NOT NULL,
+  `brgy` varchar(55) NOT NULL,
+  `city` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stud_address`
+--
+
+INSERT INTO `stud_address` (`id`, `reg_num`, `zip_code`, `houseNo`, `brgy`, `city`) VALUES
+(2, 'REG202202', 1119, 56, 'Matandang Balara', 'Quezon'),
+(3, 'REG202201', 1111, 4242, 'wadaw', 'awda'),
+(5, 'REG202203', 1111, 69, 'Hindi ko makita', 'Qc');
 
 -- --------------------------------------------------------
 
@@ -146,25 +191,22 @@ CREATE TABLE `stud_application` (
   `Age` int(2) NOT NULL,
   `Sex` varchar(6) NOT NULL,
   `Civil_Status` varchar(8) NOT NULL,
+  `Nationality` varchar(55) NOT NULL,
   `Religion` varchar(25) NOT NULL,
   `Mobile_Number` varchar(11) NOT NULL,
-  `email` varchar(55) NOT NULL
+  `email` varchar(55) NOT NULL,
+  `dor` varchar(55) NOT NULL,
+  `month` varchar(55) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stud_application`
 --
 
-INSERT INTO `stud_application` (`id`, `reg_num`, `Last_Name`, `First_Name`, `Middle_Name`, `Extension_Name`, `Complete_Address`, `Birthdate`, `Birth_Place`, `Age`, `Sex`, `Civil_Status`, `Religion`, `Mobile_Number`, `email`) VALUES
-(1, 'REG202301', 'Eballes', 'Marlon', 'U.', 'N/A', '043 Eulogio St. Don Fabian Barangay Commonwealth Quezon City', '2001-01-01', 'Quezon City', 21, 'Male', 'Single', 'Roman Catholic', '09972760119', 'marlon.union.eballes@gmail.com'),
-(3, 'REG202302', 'Bernardo', 'Emiljhon', 'M.', 'N/A', '12f2 BAYES ST. BRGY. GULOD NOVALICHES QC.', '2001-03-14', 'Manila', 21, 'Male', 'Single', 'Roman Catholic', '09498662446', 'emiljhon.bernardo@gmail.com'),
-(4, 'REG202303', 'Camaya', 'Jovilyn', 'A.', 'N/A', 'Brgy. Bagbag, Novaliches QC. ', '2001-06-21', 'Apalit, Pampanga', 20, 'Female', 'Single', 'Roman Catholic', '09229517563', 'jovilyn.almario.camaya@gmail.com'),
-(5, 'REG202304', 'Ampo', 'Mark Anthony', 'M.', 'N/A', '#31 Saint Anthony Street Republic Avenue Barangay Holy Spirit Quezon City', '2001-06-30', 'Quezon City', 20, 'Male', 'Single', 'Roman Catholic', '09275616645', 'mark.anthony.mejos.ampo@gmail.com'),
-(6, 'REG202305', 'Asuan', 'Mark Jay', 'C.', 'N/A', '66 Saint Anthony street barangay holy spirit QC ', '2000-02-26', 'Quezon City', 22, 'Male', 'Single', 'Roman Catholic', '09465209510', 'markjaybabyhawks@gmail.com'),
-(7, 'REG202306', 'Taopo', 'Cherry Ann', 'L.', 'N/A', '125 Area 6 Sitio Cabuyao Sauyo Quezon City', '1998-11-14', 'San Miguel, Bulacan', 23, 'Female', 'Single', 'Roman Catholic', '09776021403', 'cherry.ann.taopo.111598@gmail.com'),
-(8, 'REG202307', 'Rosal ', 'Erica', 'L.', 'N/A', '021 Bai Compound, Brgy. Vasra, Quezon City ', '2000-09-01', ' Negros Occidental ', 21, 'Female', 'Single', 'Christian ', '09100487940', 'erica.rosal09012000@gmail.com\r\n'),
-(9, 'REG202308', 'Galupo', 'Rica Mae', 'L.', 'N/A', '#2339 Kapatiran St. Litex rd. Brgy. Comm Q.C', '2000-04-29', 'Caloocan City', 21, 'Female', 'Single', 'Roman Catholic', '09099573850', 'galuporicamae@gmail.com'),
-(10, 'REG202309', 'Hafalla', 'Johnry', 'Y.', 'N/A', 'Alton St. Batasan Hills Q.C', '2000-03-05', 'Manila', 22, 'Male', 'Single', 'Roman Catholic', '09193415679', 'johnry.yape.hafalla@gmail.com');
+INSERT INTO `stud_application` (`id`, `reg_num`, `Last_Name`, `First_Name`, `Middle_Name`, `Extension_Name`, `Complete_Address`, `Birthdate`, `Birth_Place`, `Age`, `Sex`, `Civil_Status`, `Nationality`, `Religion`, `Mobile_Number`, `email`, `dor`, `month`) VALUES
+(26, 'REG202201', 'Bacabis', 'Mark Melvin', 'E', '', '4242 wadaw awda 1111', '2022-05-18', 'awdaw', 22, 'M', 'Married', 'awdawwadaw', 'awdawdaw', '09413123131', ' mark.melvin.bacabis@gmail.com', '2022-05-13', 'May'),
+(27, 'REG202202', 'Eballes', 'Marlon', 'Union', '', '4242 wadaw awda 1111', '2022-05-18', 'awdaw', 21, 'M', 'Married', 'awdawwadaw', 'awdawdaw', '09413123131', ' marlon.union.eballes@gmail.com', '2022-05-13', 'May'),
+(28, 'REG202203', 'Bernardo', 'Ejhay', 'ikaw na lang mamili', '', '69  Hindi ko makita Qc 1111', '2000-01-06', 'Quezon City', 21, 'M', 'Single', 'Filipino', 'Catholic', '09213123343', ' emiljhon.bernardo@gmail.com', '2022-05-13', 'May');
 
 -- --------------------------------------------------------
 
@@ -176,19 +218,20 @@ CREATE TABLE `stud_documents` (
   `id` int(11) NOT NULL,
   `reg_num` varchar(55) NOT NULL,
   `email` varchar(55) NOT NULL,
-  `diploma` varchar(55) NOT NULL,
-  `gradeslip` varchar(55) NOT NULL,
-  `id_front` varchar(55) NOT NULL,
-  `id_back` varchar(55) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `diploma` varchar(255) NOT NULL,
+  `good_moral` varchar(255) NOT NULL,
+  `grade` varchar(255) NOT NULL,
+  `id_pic` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `stud_documents`
 --
 
-INSERT INTO `stud_documents` (`id`, `reg_num`, `email`, `diploma`, `gradeslip`, `id_front`, `id_back`) VALUES
-(1, 'REG202301', 'marlon.union.eballes@gmail.com', 'sample.jpg', 'sample.jpg', 'sample.jpg', 'sample.jpg'),
-(3, 'REG202302', 'emiljhon.bernardo@gmail.com', 'sample.jpg', 'sample1.jpg', 'sample1.jpg', 'sample.jpg');
+INSERT INTO `stud_documents` (`id`, `reg_num`, `email`, `diploma`, `good_moral`, `grade`, `id_pic`) VALUES
+(15, 'REG202201', 'mark.melvin.bacabis@gmail.com', 'diploma627db232511ab4.22466283.jpg', 'gdMoral627db232511bc3.10764799.png', 'grade627db232511b91.90038550.png', 'id2x2627db232511bf7.67333543.jpg'),
+(16, 'REG202202', 'marlon.union.eballes@gmail.com', 'diploma627db25924c6c6.32712684.jpg', 'gdMoral627db25924c842.56835165.png', 'grade627db25924c816.18771746.png', 'id2x2627db25924c876.21480201.jpg'),
+(17, 'REG202203', 'emiljhon.bernardo@gmail.com', 'diploma627ddaa8d376d6.53067581.jpg', 'gdMoral627ddaa8d38127.74930623.png', 'grade627ddaa8d380d4.87222288.png', 'id2x2627ddaa8d38151.53016452.jpg');
 
 -- --------------------------------------------------------
 
@@ -207,8 +250,9 @@ CREATE TABLE `stud_sched` (
 --
 
 INSERT INTO `stud_sched` (`id`, `reg_num`, `schedID`) VALUES
-(1, 'REG202301', 1),
-(2, 'REG202302', 1);
+(19, 'REG202201', 4),
+(20, 'REG202202', 5),
+(21, 'REG202203', 4);
 
 -- --------------------------------------------------------
 
@@ -227,8 +271,9 @@ CREATE TABLE `stud_status` (
 --
 
 INSERT INTO `stud_status` (`id`, `reg_num`, `status`) VALUES
-(1, 'REG202301', 'Pending'),
-(2, 'REG202302', 'Pending');
+(20, 'REG202201', 'Approved'),
+(21, 'REG202202', 'Declined'),
+(22, 'REG202203', 'Approved');
 
 --
 -- Indexes for dumped tables
@@ -244,6 +289,12 @@ ALTER TABLE `admin_account`
 -- Indexes for table `admin_activity`
 --
 ALTER TABLE `admin_activity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `applicant_status`
+--
+ALTER TABLE `applicant_status`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -265,11 +316,16 @@ ALTER TABLE `stud_account`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `stud_address`
+--
+ALTER TABLE `stud_address`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `stud_application`
 --
 ALTER TABLE `stud_application`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `stud_documents`
@@ -297,7 +353,7 @@ ALTER TABLE `stud_status`
 -- AUTO_INCREMENT for table `admin_account`
 --
 ALTER TABLE `admin_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1500022;
 
 --
 -- AUTO_INCREMENT for table `admin_activity`
@@ -306,50 +362,58 @@ ALTER TABLE `admin_activity`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `applicant_status`
+--
+ALTER TABLE `applicant_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `approved_applicant`
 --
 ALTER TABLE `approved_applicant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `schedID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `stud_account`
 --
 ALTER TABLE `stud_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `stud_address`
+--
+ALTER TABLE `stud_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `stud_application`
 --
 ALTER TABLE `stud_application`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `stud_documents`
 --
 ALTER TABLE `stud_documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `stud_sched`
 --
 ALTER TABLE `stud_sched`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `stud_status`
 --
 ALTER TABLE `stud_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `stud_documents`
---
-ALTER TABLE `stud_documents`
-  ADD CONSTRAINT `stud_documents_ibfk_1` FOREIGN KEY (`email`) REFERENCES `stud_application` (`email`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
