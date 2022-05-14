ac2cb3e6e91f1522c421cbd48013219c53efd7ca
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-15 01:04:36 +0800

    online db

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,5 +1,6 @@
 <?php
-   $con = mysqli_connect('localhost', 'root', '', 'qcu_ereg');
+   //$con = mysqli_connect('localhost', 'root', '', 'qcu_ereg');
+   $con = mysqli_connect('sql6.freesqldatabase.com', 'sql6492227', 'MxBI2glBpC', 'sql6492227');
 
    if(!$con){
       echo "Connection failed";
